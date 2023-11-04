be1880805e02e647e5d2c3a8767e2809d3a18db7
Simon Pino-Buisson <98675268+spynob@users.noreply.github.com>
2023-11-03 23:54:51 -0400

    HomeController methods implemented

--- QuantumSlots/src/Controller/HomeController.java
+++ QuantumSlots/src/Controller/HomeController.java
@@ -1,8 +1,27 @@
 package Controller;
 
 import Model.Player;
+import Model.Potentials;
+import Potentials.PotentialFunction;
+import Service.RewardService;
 
 public class HomeController {
     private Player player;
+    private RewardService rewardService;
+    private PotentialFunction potential;
+
+    public void placeBet(int bet){
+        player.setBet(bet);
+    }
+
+    public void selectRange(float selector1, float selector2){
+    }
+
+    public void tick(){
+
+    }
+
+    public void changePotential(Potentials potential){}
+    public void measure(){}
 
 }
