fcd3da65f2e37d83e55060a388f81c45a40e67c1
LouisBouch <louisbouchard@mail.com>
2023-11-04 01:22:21 -0400

    Potential distribution function start

--- QuantumSlots/src/Potentials/InfiniteSquareWell.java
+++ QuantumSlots/src/Potentials/InfiniteSquareWell.java
@@ -2,8 +2,28 @@ package Potentials;
 
 import Model.Potentials;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class InfiniteSquareWell extends PotentialFunction {
+    double squareWellWidth;
+    /**
+     * Default constructor for the infinite square well.
+     * It creates a wave function with only the first eigenbasis
+     */
     public InfiniteSquareWell() {
         this.potentialStructure = Potentials.InfiniteSquareWell;
+        this.basisFunctions = new ArrayList<>(1);
+        squareWellWidth = 1;
+    }
+    @Override
+    public double evaluate(double position, double time) {
+        return 0;
+    }
+
+    @Override
+    public double eigenBasis(double position, int basisNumber) {
+        return 0;
     }
 }
